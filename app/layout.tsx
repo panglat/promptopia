1d8d89fb372268bd165444e5f8ec70c530e768eb
Pablo Anglat <panglat@gmail.com>
2023-07-04 11:39:57 -0300

    Home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,28 @@
+import Nav from '@components/Nav';
 import { ReactNode } from 'react';
+
 import 'styles/globals.css';
+
+type Props = {
+  children?: ReactNode;
+};
 
 export const metadata = {
   title: 'Promptopia',
   description: 'Discover & Share AI Prompts'
 };
 
-type Props = { children?: ReactNode };
-
 const RootLayout = ({ children }: Props) => {
   return (
     <html lang="en">
       <body>
         <div className="main">
-          <div className="gradient"></div>
+          <div className="gradient" />
         </div>
-        <main className="app">{children}</main>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
       </body>
     </html>
   );
---
+++ components/Provider.tsx
@@ -0,0 +1,11 @@
+import { Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
+import React, { ReactNode } from 'react';
+
+type Props = { children?: ReactNode; session?: Session };
+
+const Provider = ({ children, session }: Props) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
+};
+
+export default Provider;
